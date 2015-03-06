7aa899fb522d9dd6538eba7c777da39ea9cae453
orceN2k <dominic.schuermann@bluewin.ch>
2015-03-06 09:48:49 +0100

    add contour centroid evaluation to picture centroid

--- objectDetectionContoursFoto.py
+++ objectDetectionContoursFoto.py
@@ -14,12 +14,18 @@ threshold_maxval_180 = 180
 red = (0, 0, 255, 0)
 
 # origin image is too big, defines a scalefactor to smaller the picture
-scaleFactor = 0.4
+scaleFactor = 1
 
 # Bin width = 500px -> declare as max
 # Bin width on left or right side is +/- 300px
 binRectangleAreaMin = 300 * 250 * scaleFactor
 binRectangleAreaMax = 500 * 250 * scaleFactor
+
+# Centroids
+pictureCentroidX = 0
+pictureCentroidY = 0
+contourCentroidX = 0
+contourCentroidY = 0
 
 
 
@@ -38,31 +44,50 @@ if __name__ == '__main__':
     # Take each frame
     while True:
 
-        image = cv2.imread('right.jpg')
-        resizeImage = cv2.resize(image, (0, 0), fx=scaleFactor, fy=scaleFactor)
-        cropImage = image[1200:1450, 350:2242]
-        processingImage = cv2.resize(cropImage, (0, 0), fx=scaleFactor, fy=scaleFactor)
+        resizeImage = cv2.imread('left.jpg')
+    #    fullImage = cv2.resize(fullImageRaw, (0,0), fx=0.3, fy=0.3)
+      #  resizeImage = cv2.resize(image, (0, 0), fx=scaleFactor, fy=scaleFactor)
+        processingImage = resizeImage[1200:1450, 350:2242]
+        referenceImage = processingImage.copy()
+     #   processingImage = cv2.resize(cropImage, (0, 0), fx=scaleFactor, fy=scaleFactor)
      #   frame = cv2.resize(image, (0,0), fx=0.3, fy=0.3)
 
         # convert to greyscale
-        bw_image = cv2.cvtColor(processingImage, cv2.COLOR_BGR2GRAY)
+        processingGrey = cv2.cvtColor(processingImage, cv2.COLOR_BGR2GRAY)
+        referenceGrey = cv2.cvtColor(referenceImage, cv2.COLOR_BGR2GRAY)
 
         # invert picture
-        wb_image = cv2.bitwise_not(bw_image)
-        wb2_image = wb_image.copy()
+        processingGreyInv = cv2.bitwise_not(processingGrey)
+        processingGreyInvCopy = processingGreyInv.copy()
+        referenceGreyInv = cv2.bitwise_not(referenceGrey)
 
         # create binary picture
-        ret, binaryImage1 = cv2.threshold(wb2_image, cv2.getTrackbarPos('Threshold Value', 'window'), 255, cv2.THRESH_BINARY)
-        ret, binaryImage2 = cv2.threshold(wb2_image, cv2.getTrackbarPos('Threshold Value', 'window'), 255, cv2.THRESH_BINARY)
-        ret, binaryImage3 = cv2.threshold(wb2_image, cv2.getTrackbarPos('Threshold Value', 'window'), 255, cv2.THRESH_BINARY)
+        ret, binaryImage1 = cv2.threshold(processingGreyInvCopy, cv2.getTrackbarPos('Threshold Value', 'window'), 255, cv2.THRESH_BINARY)
+        ret, binaryImage2 = cv2.threshold(processingGreyInvCopy, cv2.getTrackbarPos('Threshold Value', 'window'), 255, cv2.THRESH_BINARY)
+        ret, binaryImage3 = cv2.threshold(processingGreyInvCopy, cv2.getTrackbarPos('Threshold Value', 'window'), 255, cv2.THRESH_BINARY)
+
+        ret, referenceBinary = cv2.threshold(referenceGreyInv, 0, 255, cv2.THRESH_BINARY)
 
         # filtering
         kernel = np.ones((8, 8), np.uint8)
         filteredBinaryImage = cv2.morphologyEx(binaryImage3, cv2.MORPH_OPEN, kernel)
         filteredBinaryImage2 = filteredBinaryImage.copy()
 
+        # evaluate full picture centroid
+        referenceContours, fullHierarchy = cv2.findContours(referenceBinary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+        for cnt in referenceContours:
 
+            # get centroid of whole picture, base reference for angle correction
+            fullMoments = cv2.moments(cnt)
+            fullCentroidX = int(fullMoments['m10']/fullMoments['m00'])
+            fullCentroidY = int(fullMoments['m01']/fullMoments['m00'])
+            cv2.circle(referenceImage, (fullCentroidX, fullCentroidY), 3, (0, 0, 255))
+            print(fullCentroidX)
+            pictureCentroidX = fullCentroidX
+            pictureCentroidY = fullCentroidY
+
+        # evaluate contours on picture -> find bin
         contours2, hierarchy2 = cv2.findContours(filteredBinaryImage2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         for cnt in contours2:
@@ -74,38 +99,46 @@ if __name__ == '__main__':
          #   print(contourArea)
          #   print(binRectangleAreaMin)
          #   print(binRectangleAreaMax)
-            if 5000 < contourArea < 20000:
+            if binRectangleAreaMin < contourArea < binRectangleAreaMax:
             #    print(contourArea)
                 cv2.drawContours(processingImage, [cnt], 0, (0, 255, 0), 2)
 
                 hull = cv2.convexHull(cnt)
-                print(hull)
+               # print(hull)
 
                 for points in hull:
 
                     point = points[0]
-                    print(point[0], point[1])
+                 #   print(point[0], point[1])
                     cv2.circle(processingImage, (point[0], point[1]), 5, (0, 0, 255))
 
-
                 # get centroid
-                M = cv2.moments(cnt)
-                centroid_x = int(M['m10']/M['m00'])
-                centroid_y = int(M['m01']/M['m00'])
-                cv2.circle(processingImage, (centroid_x, centroid_y), 3, (0, 150, 0))
-                print(centroid_x)
+                contourMoments = cv2.moments(cnt)
+                centroidX = int(contourMoments['m10']/contourMoments['m00'])
+                centroidY = int(contourMoments['m01']/contourMoments['m00'])
+                cv2.circle(processingImage, (centroidX, centroidY), 3, (0, 150, 0))
+                print(centroidX)
+                contourCentroidX = centroidX
+                contourCentroidY = centroidY
 
-        #for cnt in contours2:
-        #    approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-        #    cv2.drawContours(frame ,[cnt], -1, (0, 255, 0), 2)
+        #calculate difference to picture centroid
+        if not (pictureCentroidX is None):
+            if not (contourCentroidX is None):
+                distanceToCentroid = pictureCentroidX - contourCentroidX
+                print(distanceToCentroid)
+
+                cv2.line(referenceImage, (pictureCentroidX, pictureCentroidY), (contourCentroidX, contourCentroidY), (0, 0, 255), 2)
+
 
      #   cv2.imshow('crop', crop_img);
         cv2.imshow('crop', processingImage)
-        cv2.imshow('original', resizeImage)
+     #   cv2.imshow('original', resizeImage)
      #   cv2.imshow('original', image)
         cv2.imshow('contours (opening2)', filteredBinaryImage2)
         cv2.imshow('binary (binaryImage2)', binaryImage2)
         cv2.imshow('after filter (opening)', filteredBinaryImage)
+        cv2.imshow('fullBinary', referenceImage)
+
     #    cv2.imshow('hsv mask', hsv_opening)
         #print(contours2)
 
