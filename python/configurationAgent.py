da0012a30774435c55ba9732e5db8b3aaf89543b
orceN2k <dominic.schuermann@bluewin.ch>
2015-05-22 10:43:28 +0200

    rename folder / clean up

---
+++ python/configurationAgent.py
@@ -0,0 +1,13 @@
+__author__ = 'orceN'
+
+import objectDetectionDom
+
+def createBinaryPicture():
+    objectDetectionDom
+
+if __name__ == '__main__':
+    print("======================================================================")
+    print("Configuration started")
+    print("----------------------------------------------------------------------")
+    print("create reference binary picture")
+    createBinaryPicture()
